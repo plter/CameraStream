5839adeaa539b77055d0934ce352f35b3509263a
plter <xtiqin@163.com>
2018-09-05 21:16:29 +0800

    实现多个摄像头可选的功能

--- renderer.js
+++ renderer.js
@@ -5,7 +5,7 @@
 class Main {
 
     constructor() {
-        this._videoInputDevices = [];
+        this._videoInputDevices = new Map();
         this.VIDEO_WIDTH = 1280;
         this.VIDEO_HEIGHT = 720;
 
@@ -19,19 +19,41 @@ class Main {
 
         devices.forEach(value => {
             if (value.kind === "videoinput") {
-                this._videoInputDevices.push(value);
+                this._videoInputDevices.set(value.deviceId, value);
             }
         });
 
-        if (this._videoInputDevices.length > 0) {
-            this.showVideo(this._videoInputDevices[0]);
+        console.log(this._videoInputDevices);
+        if (this._videoInputDevices.size > 0) {
+            this.showVideo(this._videoInputDevices.values().next());
 
-            if (this._videoInputDevices.length > 1) {
-                //TODO add select
+            if (this._videoInputDevices.size > 1) {
+                this.addSelect(this._videoInputDevices);
             }
         } else {
             alert("没有找到摄像头");
         }
+    }
+
+    /**
+     *
+     * @param {Map} devices
+     */
+    addSelect(devices) {
+        let select = document.createElement("select");
+        select.style.position = "fixed";
+        select.style.left = "0";
+        select.style.top = "0";
+        document.body.appendChild(select);
+        devices.forEach((value, key) => {
+            let op = document.createElement("option");
+            op.innerHTML = value.label;
+            op.value = value.deviceId;
+            select.appendChild(op);
+        });
+        select.onchange = e => {
+            this.showVideo(this._videoInputDevices.get(select.value));
+        };
     }
 
     initUI() {
@@ -55,6 +77,7 @@ class Main {
             video: {
                 frameRate: 30,
                 deviceId: videoInputDevice.deviceId,
+                groupId: videoInputDevice.groupId,
                 width: this.VIDEO_WIDTH,
                 height: this.VIDEO_HEIGHT
             }
